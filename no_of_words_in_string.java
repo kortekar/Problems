bf967f6a6e86c29728584e8fab9b7fcf0fbd2be4
Chandan K Kortekar <127336792+kortekar@users.noreply.github.com>
2023-12-16 11:32:47 +0530

    no_of_words_in_string in java

---
+++ no_of_words_in_string.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+        String str;
+        int count=0;
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        String[] words = str.split("\\s+");
+        count = words.length;
+        System.out.println(count);
+        for (int i = 0; i < count; i++) {
+            System.out.print(words[i]+",");
+        }
+    }
+}
